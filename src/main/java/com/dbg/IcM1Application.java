01ad594a9f7bdcf9acc1028a18c0bc1be8be0ba8
David Borrego Gutiérrez <dborrego@atsistemas.com>
2016-09-12 12:25:50 +0200

    Iniciando repo

---
+++ src/main/java/com/dbg/IcM1Application.java
@@ -0,0 +1,12 @@
+package com.dbg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IcM1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IcM1Application.class, args);
+	}
+}
