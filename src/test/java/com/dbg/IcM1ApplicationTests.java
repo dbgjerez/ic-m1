99a20dd3f9fd65d076ad1f7e1bca0e2b46cd40c3
David Borrego Gutiérrez <dbgjerez@gmail.com>
2017-01-24 13:01:33 +0100

    ejecuto test con contexto global

--- src/test/java/com/dbg/IcM1ApplicationTests.java
+++ src/test/java/com/dbg/IcM1ApplicationTests.java
@@ -5,8 +5,10 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import com.dbg.ic.m1.IcM1Application;
+
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(classes = IcM1Application.class)
 public class IcM1ApplicationTests {
 
 	@Test
